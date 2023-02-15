c2f3cbc0783a1f149a0d46c26a8e33845b5509f0
AndreaBollino <bollino.andrea@gmail.com>
2023-02-15 16:24:18 +0100

    Update ROBO4_filtro_NDC_Indice.py

--- ROBO4_filtro_NDC_Indice.py
+++ ROBO4_filtro_NDC_Indice.py
@@ -67,27 +67,23 @@ def countPresenti(jwt,listaXLS):
     value = data2['count']
     #print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(value))
     return value
-def updateNDCstrategiaNULL():
+def updateNDCstrategiaNULL(listaxls):
     url = 'https://digitaladvisory-elk.coding.mps.apps.paas.testfactory.copergmps:443/bsrobo4-risp-picking-'+indice+'/_update_by_query'
     autorization = 'Bearer'+ jwt
     autorization = autorization.replace('"', '')
     autorization = autorization.replace('}', '')
-    print(autorization)
+    #print(autorization)
     headers = {'Content-Type': 'application/json','Authorization': autorization} 
     #print (headers)
-    data = {
+    data ={
     "script": {
         "source": "ctx._source.strategiaAssegnata = null",
         "lang": "painless"
-    },
+     },
     "query": {
-        "terms": {
-            "ndg": [
- #               "5799"
-            ]
-        }
+        "terms": {"ndg": listaxls}
+     }
     }
-}
     response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
     #print('INDICE: ' + response.text)
     #data2 = response.json() 
@@ -103,31 +99,38 @@ countNDCad1SuIndice = countNDCad1(jwt)
 #updateNDCstrategiaNULL()
 filexls = input("Immetti nome folgio xls filtro : ")  
 
-#leggiamo il foglio excel e salviamo i dati in una variabile
 #data = pd.read_excel('CA Fondi Finestra_bsrobo4-risp-picking-1669 - esclusioni.xlsx') 
 data = pd.read_excel(filexls) 
-#data = pd.read_excel(filexls) 
-#salviamo la prima colonna del foglio in una lista 
 lista = data.iloc[:,0].tolist() 
 lista[:] = np.unique(lista)
 output = [str(x) for x in lista]
-countNDCxlsStr1 = countPresenti(jwt,output)
+outputOrig = [str(x) for x in lista]
+#per ogni elemento della lista, sostituiamolo con se stesso concatenato con +
+risposta= input('vuoi preparare file txt con NDC pronti per Postman? s/n ')
+if (risposta=='s'):
+    with open('modificato.txt', 'w') as outfile:
+        for i in range(len(output)-1):  #scorriamo tutti gli elementi della lista  
+            output[i] = '"' + output[i] + '"' + ';' #sostituisci l'elemento con se stesso concatenato con +  
+            print(output[i])
+            outfile.write(output[i]+"\n")
+
+        output[len(output)-1] = '"' + output[len(output)-1] + '"' 
+        print(output[len(output)-1])
+        outfile.write(output[len(output)-1])
+        #print(len(output))
+        outfile.close()
+ciccio= input('vai avanti')
+countNDCxlsStr1 = countPresenti(jwt,outputOrig)
 print('Presenza NDC su INDICE e strategia ad 1: ' + str(countNDCad1SuIndice))
-print('NDC da elliminare: ' + str(len(output)))
+print('NDC da elliminare: ' + str(len(outputOrig)))
 print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countNDCxlsStr1))
-print('NDC rimanenti dopo aver Filtrato= ' + str(countNDCxlsStr1-len(output)))
+print('NDC rimanenti dopo aver Filtrato= ' + str(countNDCxlsStr1-len(outputOrig)))
+pippo=input('Dai ok per update')
+risposta = updateNDCstrategiaNULL(outputOrig)
+print('UPDATE eseguito messa strategia a null su NDC dell Indice presenti in foglio xls')
+print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countPresenti(jwt,outputOrig)))
+print('Presenza NDC su INDICE e strategia ad 1: ' + str(countNDCad1(jwt)))
+
 esci=input()
 
-#per ogni elemento della lista, sostituiamolo con se stesso concatenato con + 
-with open('modificato.txt', 'w') as outfile:
-    for i in range(len(output)-1):  #scorriamo tutti gli elementi della lista  
-        output[i] = '"' + output[i] + '"' + ';' #sostituisci l'elemento con se stesso concatenato con +  
-        print(output[i])
-        outfile.write(output[i]+"\n")
 
-    output[len(output)-1] = '"' + output[len(output)-1] + '"' 
-    print(output[len(output)-1])
-    outfile.write(output[len(output)-1])
-    #print(len(output))
-    outfile.close()
-ciccio= input()
