fcd591cddf581f9c1470de29bfa918f6f283228f
AndreaBollino <bollino.andrea@gmail.com>
2023-06-16 09:52:57 +0200

    nuova versione
    nuova versione

--- esempio_DB.py
+++ esempio_DB.py
@@ -6,7 +6,6 @@ pw = os.environ.get('PASSWORD')
 
 cx_Oracle.init_oracle_client(config_dir="C:\\oracle\\ora12R2CL_32\\network\\admin")
 connection = cx_Oracle.connect(user=un, password=pw, dsn="OraWV_NPWAADVISORYAPP_PROD.world", encoding="UTF-8")
-
 cursor = connection.cursor()
 query = "select * from NPWAPFP.tbl_guida_staging"
 cursor.execute(query)
