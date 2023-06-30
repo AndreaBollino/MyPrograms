31b7e8e316faf78d0692af807a93b9bc8915590c
AndreaBollino <bollino.andrea@gmail.com>
2023-06-30 15:03:41 +0200

    modifiche

---
+++ robo4/ROBO4_filtro_NDC_Indice.py
@@ -0,0 +1,163 @@
+#elasticsearch costruisci query terms in and multisearch
+#importiamo la libreria pandas per leggere i fogli excel
+#scrivi programma python per chiamata rest post  di esempio con body in formato json
+#scrivi programma python che ellimina " da una stringa 
+#in python come posso cercare il valore di un campo nella response di una chimata rest?
+
+#conda install -c anaconda pandas
+import pandas as pd 
+import requests
+import json
+import numpy as np
+import time
+import os
+import getpass
+
+
+def login():
+    url = 'https://idpint.sum.gmps.global/sbopenamrest/api/oidcappservice'
+    headers = {'Content-Type': 'application/json'} 
+    data = {
+        #"username":"APPWVELK",
+        "username":user,
+        #"password":"1_h849fnZuiNKocUhXDu5J9w==",
+        "password":passwd,
+        "clientid":"digitaladv",
+        "clientsecret":"1_wItFnJhk211TKX/WalIQ9A==",
+        #"fgPswEncrypted":"true"
+        "fgPswEncrypted":"false",
+        "scope":"roles"
+}
+    response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+    jwt=response.text.split(':')[1]
+    print('JWT:' + jwt)
+    return jwt
+
+def countNDCad1(jwt):
+    url = 'https://digitaladvisory-elk-wvk8snpwaelasticsearch-sysprod.apps.fi1.paas.gmps.global:443/bsrobo4-risp-picking-'+indice+'/_count'
+    autorization = 'Bearer'+ jwt
+    autorization = autorization.replace('"', '')
+    autorization = autorization.replace('}', '')
+    #print(autorization)
+    headers = {'Content-Type': 'application/json','Authorization': autorization} 
+    #print (headers)
+    data = {"query":{"term":{"strategiaAssegnata":{"value":"1"}}}}
+    response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+    #print('INDICE: ' + response.text)
+    data2 = json.loads(response.text)
+    value = data2['count']
+    #print('Numero NDC con Strategia ad 1 su INDICE: ' + str(value))
+    return value
+    
+def countPresenti(jwt,listaXLS):
+    url = 'https://digitaladvisory-elk-wvk8snpwaelasticsearch-sysprod.apps.fi1.paas.gmps.global:443/bsrobo4-risp-picking-'+indice+'/_count'
+    autorization = 'Bearer'+ jwt
+    autorization = autorization.replace('"', '')
+    autorization = autorization.replace('}', '')
+    #print(autorization)
+    headers = {'Content-Type': 'application/json','Authorization': autorization} 
+    #print (headers)
+    #https://stackoverflow.com/questions/43633472/how-to-simulate-multiple-fields-in-a-terms-query
+    data ={ 
+    "query": { 
+        "bool": { 
+            "must": [ 
+                {"term": {"strategiaAssegnata":{"value":"1"}}}, 
+                {"terms": {"ndg": listaXLS}}
+            ] 
+        } 
+    } 
+}
+    response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+    #data2 = response.json() 
+    data2 = json.loads(response.text)
+    value = data2['count']
+    #print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(value))
+    return value
+def updateNDCstrategiaNULL(listaxls):
+    url = 'https://digitaladvisory-elk-wvk8snpwaelasticsearch-sysprod.apps.fi1.paas.gmps.global:443/bsrobo4-risp-picking-'+indice+'/_update_by_query'
+    autorization = 'Bearer'+ jwt
+    autorization = autorization.replace('"', '')
+    autorization = autorization.replace('}', '')
+    #print(autorization)
+    headers = {'Content-Type': 'application/json','Authorization': autorization} 
+    #print (headers)
+    data ={
+    "script": {
+        "source": "ctx._source.strategiaAssegnata = null",
+        "lang": "painless"
+     },
+    "query": {
+        "terms": {"ndg": listaxls}
+     }
+    }
+    #try: 
+    response = requests.post(url, headers=headers, data=json.dumps(data), verify=False)
+    #except requests.exceptions.RequestException as e: 
+    #    print('An exception has occurred: %s' % e)
+    #print('INDICE: ' + response.text)
+    #data2 = response.json() 
+    #value = data2['count']
+    print('NDC da lista xls messi Strategia a null: ' + str(response))
+    return response.status_code
+
+user = os.environ.get("USERNAME")
+print ("Utente: "+user)
+passwd = getpass.getpass("Inserisci la tua password: ")
+
+indice=input("Immetti numero indice : ")   
+indice = str(indice)       
+jwt = login()
+countNDCad1SuIndice = countNDCad1(jwt)
+print (countNDCad1SuIndice)
+#updateNDCstrategiaNULL()
+filexls = input("Immetti nome folgio xls filtro : ")  
+
+#data = pd.read_excel('CA Fondi Finestra_bsrobo4-risp-picking-1669 - esclusioni.xlsx') 
+data = pd.read_excel(filexls) 
+lista = data.iloc[:,0].tolist() 
+lista[:] = np.unique(lista)
+output = [str(x) for x in lista]
+outputOrig = [str(x) for x in lista]
+#per ogni elemento della lista, sostituiamolo con se stesso concatenato con +
+risposta= input('vuoi preparare file txt con NDC pronti per Postman? s/n ')
+if (risposta=='s'):
+    with open('modificato.txt', 'w') as outfile:
+        for i in range(len(output)-1):  #scorriamo tutti gli elementi della lista  
+            output[i] = '"' + output[i] + '"' + ';' #sostituisci l'elemento con se stesso concatenato con +  
+            print(output[i])
+            outfile.write(output[i]+"\n")
+
+        output[len(output)-1] = '"' + output[len(output)-1] + '"' 
+        print(output[len(output)-1])
+        outfile.write(output[len(output)-1])
+        #print(len(output))
+        outfile.close()
+ciccio= input('vai avanti')
+countNDCxlsStr1 = countPresenti(jwt,outputOrig)
+print('Presenza NDC su INDICE e strategia ad 1: ' + str(countNDCad1SuIndice))
+print('NDC da elliminare: ' + str(len(outputOrig)))
+print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countNDCxlsStr1))
+print('NDC rimanenti dopo aver Filtrato= ' + str(countNDCxlsStr1-len(outputOrig)))
+pippo=input('Dai ok per update')
+try:
+    risposta = updateNDCstrategiaNULL(outputOrig)
+except requests.exceptions.RequestException as e: 
+    print('An exception has occurred: %s' % e)
+    print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countPresenti(jwt,outputOrig)))
+    print('Presenza NDC su INDICE e strategia ad 1: ' + str(countNDCad1(jwt)))
+    esci=input()
+    exit()
+if (risposta == 200):
+    print('UPDATE eseguito messa strategia a null su NDC dell Indice presenti in foglio xls')
+    print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countPresenti(jwt,outputOrig)))
+    print('Presenza NDC su INDICE e strategia ad 1: ' + str(countNDCad1(jwt)))
+else:
+    ndcAD1 = countPresenti(jwt,outputOrig)
+    while ndcAD1 != 0: 
+            print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(ndcAD1))
+            time.sleep(10)
+            ndcAD1 = countPresenti(jwt,outputOrig)
+    print('Presenza NDC da foglio xls su INDICE e strategia ad 1: ' + str(countPresenti(jwt,outputOrig)))
+
+esci=input()
---
+++ robo4/query_DB_suCSV_XLS.py
@@ -0,0 +1,92 @@
+import cx_Oracle
+#import os
+import csv
+from openpyxl import Workbook
+
+#un = os.environ.get('USER_NAME')
+#pw = os.environ.get('PASSWORD')
+
+cx_Oracle.init_oracle_client(config_dir="C:\\oracle\\ora12R2CL_32\\network\\admin")
+connection = cx_Oracle.connect(dsn="OraWV_NPWAADVISORYAPP_PROD.world", encoding="UTF-8")
+cursor = connection.cursor()
+query = """
+WITH totale AS
+  (SELECT r4s.DESCRIPTION AS R4_SESSION_DESCRIPTION,
+    s.INDICE_OUTPUT       AS BS_SESSION_SCHED_INDICE_OUTPUT,
+    r4t.id                AS R4_TARGET_ID,
+    bsw.START_DATE        AS data_partenza
+  FROM NPWACDR4.BS_SESSION_SCHED s
+  INNER JOIN Npwacdr4.Bs_Session_Progress P
+  ON P.Id_Session_Sched = S.Id
+  LEFT JOIN NPWACDR4.R4_TARGET r4t
+  ON r4t.id = s.CODICE_SESSIONE
+  LEFT JOIN NPWACDR4.R4_SESSION r4s
+  ON r4s.id = r4t.SESSION_ID
+  JOIN NPWACDR4.BS_SESSION_WORKER_PROGRESS bsw
+  ON p.id              = bsw.ID_SESSION_PROGRESS
+  WHERE 1              =1
+  AND r4s.DESCRIPTION IS NOT NULL
+  AND (R4s.Description in 'CA Sottopeso GOVB/CORB Giu 23 - Personas BOND' or
+       R4s.Description in 'CA Sottopeso GOVB/CORB Giu 23 - Personas BOND' )
+  --LIKE 'CA Top di Gamma Aderenza < 7 LUG-AGO 23'
+  --AND R4s.Description LIKE :description
+    --and s.INDICE_OUTPUT  in ('bsrobo4-risp-picking-2507','bsrobo4-risp-picking-2506')
+    --order by r4t.id desc-- as R4_TARGET_ID
+  ORDER BY bsw.START_DATE DESC
+  )
+SELECT R4_SESSION_DESCRIPTION,
+  BS_SESSION_SCHED_INDICE_OUTPUT,
+  R4_TARGET_ID
+FROM totale
+GROUP BY R4_SESSION_DESCRIPTION,
+  BS_SESSION_SCHED_INDICE_OUTPUT,
+  R4_TARGET_ID
+"""
+cursor.execute(query)
+
+# Recupero dei risultati
+results = cursor.fetchall()
+
+# Estrazione dei nomi delle colonne
+column_names = [col[0] for col in cursor.description]
+
+# Lettura dei risultati
+for row in results:
+    print(row)
+
+output_file = "query.csv"
+
+# Scrittura dei risultati su un file CSV
+with open(output_file, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile, delimiter=";")
+    
+    # Scrittura dei nomi delle colonne come prima riga
+    writer.writerow(column_names)
+
+    # Scrittura dei dati
+    for row in results:
+        writer.writerow(row)
+
+# Creazione del foglio di lavoro Excel
+workbook = Workbook()
+sheet = workbook.active
+
+# Scrittura dei nomi delle colonne nel foglio di lavoro
+for col_idx, column_name in enumerate(column_names, start=1):
+    sheet.cell(row=1, column=col_idx, value=column_name)
+
+# Scrittura dei risultati della query nel foglio di lavoro
+row_idx = 2
+for row in results:
+    for col_idx, value in enumerate(row, start=1):
+        sheet.cell(row=row_idx, column=col_idx, value=value)
+    row_idx += 1
+
+# Salvataggio del foglio di lavoro
+workbook.save('query2.xlsx')
+
+# Chiusura della connessione
+cursor.close()
+connection.close()
+
+print("Query eseguita con successo. I risultati sono stati scritti su", output_file)
