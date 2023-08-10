8824d42427c04c78bbc810ce3df02da872c99b8a
AndreaBollino <bollino.andrea@gmail.com>
2023-08-10 17:18:28 +0200

    mon

---
+++ MON/estrazione49.py
@@ -0,0 +1,50 @@
+import csv
+def leggi_valori_da_file(file_path):
+    with open(file_path, 'r') as file:
+        valori = set(line.strip() for line in file)
+    return valori
+
+def estrai_righe_da_file(input_file_path, output_file_path, valori_da_trovare):
+    #filtered_rows = []
+    with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
+        for line in input_file:
+            if any(valore in line for valore in valori_da_trovare):
+                output_file.write(line)
+
+def ordina_file_per_ottavo_elemento(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file, delimiter=';')
+        #header = next(reader)
+        sorted_rows = sorted(reader, key=lambda row: int(row[7]))  # Ordina per ottavo elemento (indice 7)
+        print(sorted_rows)
+    
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file, delimiter=';')
+        #writer.writerow(header)
+        writer.writerows(sorted_rows)
+
+def main():
+    # Percorso del file contenente i valori da cercare
+    valori_file_path = 'indici49.txt'
+    valori_da_trovare = leggi_valori_da_file(valori_file_path)
+
+    # Percorsi dei file di input e output
+    input_file_path = 'file_guida_sisco.csv'
+    output_file_path = 'file_guida_sisco_new.csv'
+
+    # Estrai le righe dal file di input e scrivile nel nuovo file
+    estrai_righe_da_file(input_file_path, output_file_path, valori_da_trovare)
+    #ordina_file_per_ottavo_elemento("file_guida_sisco_new.csv")
+    with open('file_guida_sisco.csv', 'r') as file_guida_originale:
+        prima_riga = next(file_guida_originale)
+        with open('file_guida_sisco_new.csv', 'r+') as file_guida_nuovo:
+            contenuto = file_guida_nuovo.read()
+            file_guida_nuovo.seek(0, 0)
+            file_guida_nuovo.write(prima_riga.rstrip('\r\n') + '\n' + contenuto)
+    
+    #ordina_file_per_ottava_colonna("file_guida_sisco_new.csv")
+    #ottavi_elementi = estrai_ottavo_elemento('file_guida_sisco_new.csv')
+    #print(ottavi_elementi)
+
+if __name__ == "__main__":
+    main()
---
+++ MON/pdf49.py
@@ -0,0 +1,32 @@
+import os
+
+# Leggi il valore dal file indici.txt
+def leggi_valori_da_file(file_path):
+    with open(file_path, 'r') as file:
+        valori = set(line.strip() for line in file)
+    return valori
+
+valori_file_path = 'indici49.txt'
+valori_da_trovare = leggi_valori_da_file(valori_file_path)
+
+
+# Percorso alla directory di origine
+directory_origine = r"U:\1030\JForm\OutJform\00000"
+
+# Percorso alla directory di destinazione
+directory_destinazione = r"C:\Users\S511480\Desktop\Python_MieProgrammi\MyPrograms\MON\49"
+
+# Cerca i file che contengono il valore nel loro nome
+for filename in os.listdir(directory_origine):
+    #if valori_da_trovare in filename:
+    for valore in valori_da_trovare:
+                if valore in filename:
+        # Costruisci i percorsi completi per i file di origine e destinazione
+                    percorso_file_origine = os.path.join(directory_origine, filename)
+                    percorso_file_destinazione = os.path.join(directory_destinazione, filename)
+        
+        # Copia il file nella directory di destinazione
+                    with open(percorso_file_origine, "rb") as file_origine, open(percorso_file_destinazione, "wb") as file_destinazione:
+                        file_destinazione.write(file_origine.read())
+        
+                    print(f"Copiato il file {filename} nella directory di destinazione.")
